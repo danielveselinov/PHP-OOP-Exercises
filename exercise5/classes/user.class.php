565a6d86c424ca47022c7a4e722c88ea8f0460fc
Daniel Veselinov <daniel@veselinov.com>
2022-03-06 16:27:24 +0100

    Initial solution

--- exercise5/classes/user.class.php
+++ exercise5/classes/user.class.php
@@ -1,19 +1,11 @@
 <?php 
-
-class User {
+abstract class User {
     protected $name;
     protected $surname;
     protected $username;
     protected $is_admin = false;
 
-    public function __construct($name, $surname, $username, $is_admin = false)
-    {
-        $this->setName($name);
-        $this->setSurname($surname);
-        $this->setUsername($username);
-        $this->setAdmin($is_admin);
-    }
-
+    
     /**
      * Print user full name, if user=admin prints (admin)
      */
@@ -103,5 +95,92 @@ class User {
     public function getAdmin()
     {
         return $this->is_admin;
+    }
+}
+
+class Customer extends User {
+    private $city;
+    private $state;
+    private $country;
+
+    public function __construct($city, $state, $country)
+    {
+        $this->setCity($city);
+        $this->setState($state);
+        $this->setCountry($country);
+    }
+
+    public function location() {
+        echo "{$this->getCity()}, {$this->getState()}, {$this->getCountry()}";
+    }
+
+    /**
+     * Get the value of city
+     */ 
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set the value of city
+     *
+     * @return  self
+     */ 
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of state
+     */ 
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * Set the value of state
+     *
+     * @return  self
+     */ 
+    public function setState($state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of country
+     */ 
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set the value of country
+     *
+     * @return  self
+     */ 
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+}
+
+class AdminUser extends User {
+    public function __construct($name, $surname, $username, $is_admin = false)
+    {
+        $this->setName($name);
+        $this->setSurname($surname);
+        $this->setUsername($username);
+        $this->setAdmin($is_admin);
     }
 }
--- exercise5/exercise5.php
+++ exercise5/exercise5.php
@@ -1,85 +1,19 @@
 <?php  
-
 require __DIR__ . "/classes/user.class.php";
-// require __DIR__ . "/classes/customer.class.php";
 
-class Customer extends User {
-    private $city;
-    private $state;
-    private $country;
 
-    public function __construct($city, $state, $country)
-    {
-        $this->setCity($city);
-        $this->setState($state);
-        $this->setCountry($country);
-    }
+$user = new AdminUser("John", "Doe", "john@", true);
+$customerData = new Customer("Shtip", "Shtip", "Macedonia");
+$user->printFullName(); 
+echo "<br/>";
+$customerData->location();
 
-    public function location() {
-        echo "{$this->getCity()} {$this->getState()} {$this->getCountry()}";
-    }
-
-    /**
-     * Get the value of city
-     */ 
-    public function getCity()
-    {
-        return $this->city;
-    }
-
-    /**
-     * Set the value of city
-     *
-     * @return  self
-     */ 
-    public function setCity($city)
-    {
-        $this->city = $city;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of state
-     */ 
-    public function getState()
-    {
-        return $this->state;
-    }
-
-    /**
-     * Set the value of state
-     *
-     * @return  self
-     */ 
-    public function setState($state)
-    {
-        $this->state = $state;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of country
-     */ 
-    public function getCountry()
-    {
-        return $this->country;
-    }
-
-    /**
-     * Set the value of country
-     *
-     * @return  self
-     */ 
-    public function setCountry($country)
-    {
-        $this->country = $country;
-
-        return $this;
-    }
+for ($i = 0; $i < 2; $i++) {
+    echo "<hr/>";
 }
 
-//
-$user = new User("John", "Doe", "john@", true);
-$user->printFullName();
+$user2 = new AdminUser("Jane", "Smith", "_smith2000");
+$customerData2 = new Customer("Miami", "Florida", "USA");
+$user2->printFullName(); 
+echo "<br/>";
+$customerData2->location();
